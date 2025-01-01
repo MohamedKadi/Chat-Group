be4112ec54f1588e713d3cf2b376f35487687e18
MohamedKadi <gamesmohamed14@gmail.com>
2025-01-01 17:09:14 +0100

    SERVER: broadcasting msg & client handler class

--- src/Server.java
+++ src/Server.java
@@ -1,4 +1,4 @@
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.List;
@@ -14,16 +14,87 @@ public class Server {
             //creating a server
             server =new ServerSocket();
             //listening to any clients that connects
-            socket = server.accept();
-            System.out.println("new client connected");
+            while(true){
+                socket = server.accept();
+                System.out.println("new client connected");
 
-
+                ClientHandler client = new ClientHandler(socket, this);
+                clients.add(client);
+                new Thread(client).start();
+            }
         }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            closeServer();
+        }
+    }
+    public void BroadcastMsg(String msg,ClientHandler client){
+        for(ClientHandler c : clients){
+            if(c != client){
+                c.sendMsg(msg);
+            }
+        }
+    }
+
+    public void removeClient(ClientHandler c){
+        clients.remove(c);
+    }
+
+    public void closeServer(){
+        try{
+            server.close();
+            socket.close();
+
+        }catch(IOException e){
             e.printStackTrace();
         }
     }
 }
 
-class ClientHandler{
+class ClientHandler implements Runnable{
+    Server server;
+    Socket socket;
+    BufferedWriter bw;
+    BufferedReader br;
+    public ClientHandler(Socket socket, Server server){
+        this.server = server;
+        this.socket = socket;
+    }
 
+    @Override
+    public void run() {
+        try{
+            br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String msg;
+            while((msg = br.readLine()) != null){
+                System.out.println("message that the server got: "+msg);
+                server.BroadcastMsg(msg,this);
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }finally {
+            close();
+        }
+    }
+    public void sendMsg(String msg){
+        try{
+            bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            bw.write(msg);
+            bw.flush();
+        }catch(IOException e){
+            e.printStackTrace();
+        }finally {
+            close();
+        }
+    }
+    public void close() {
+        try{
+            if(socket != null) socket.close();
+            if(br != null)  br.close();
+            if(bw != null) bw.close();
+            server.removeClient(this);
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
 }
