f5a5c2793eed76aa35de0597ec17e870b5b30235
MohamedKadi <gamesmohamed14@gmail.com>
2025-01-01 18:20:06 +0100

    fixing client side and adding quit functionality

--- src/Client.java
+++ src/Client.java
@@ -10,34 +10,57 @@ public class Client implements Runnable{
     public Client(String url, int port){
         try{
             socket = new Socket(url,port);
-            System.out.print("send a message to the group");
+            System.out.println("send a message to the group");
             br = new BufferedReader(new InputStreamReader(System.in));
             bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
             new Thread(this).start();
-            String msg;
+            String msg="";
             while(true){
                 while((msg = br.readLine()) != null){
+                    if(msg.equals("quit")){
+                        System.out.println("quit");
+                        break;
+                    }
                     bw.write(msg);
                     bw.newLine();
                     bw.flush();
                 }
+                break;
             }
         }catch (UnknownHostException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
+        }finally {
+            this.close();
+
         }
     }
 
     public void listenToServer(){
         try{
             brserver = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            String msg;
-            while((msg=brserver.readLine())!=null){
-                System.out.println("some client said: "+msg);
+            String msg="";
+            while(!socket.isClosed() && (msg=brserver.readLine())!=null){
+                System.out.println("some client said: "+ msg);
             }
+
+
         } catch (IOException e) {
             e.printStackTrace();
+        }finally {
+            this.close();
+        }
+    }
+
+    public void close(){
+        try{
+            if(br != null) br.close();
+            if(bw != null) bw.close();
+            if(brserver != null) brserver.close();
+            if(socket != null) socket.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
